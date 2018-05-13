03f9c8a6ce7af8ea74501f2a97b03db0a21983be
Bartoszeg02 <Bartoszeg02@gmail.com>
2018-05-13 21:28:14 +0200

    commit03

--- js/algorytmy.js
+++ js/algorytmy.js
@@ -14,7 +14,9 @@ var input_Rainhards_shieldbearers = document.getElementById("i_number_of_shieldb
 
     button_fill_in_Galahads_army = document.getElementById("b_fill_in_Galahads_army"),
 
-    button_start_the_battle = document.getElementById("button_start_the_battle");
+    button_start_the_battle = document.getElementById("button_start_the_battle"),
+
+    main_picture = document.getElementById("main_picture_1");
 
 function view_Rainhards_army(shields, crossbows){
     var shields,
@@ -23,7 +25,7 @@ function view_Rainhards_army(shields, crossbows){
     if(!shields){
         shields = 0;
     }
-    if(!crossbows){
+    if(!crossbows){ 
         crossbows = 0;
     }  
     //Wyświetlam liczbę wojsk pod odpowiednimi grafikami
@@ -44,19 +46,34 @@ function view_Galahads_army(shields, crossbows){
     number_of_crossbowmen_G.innerHTML = crossbows;
 }
 
+
+function change_screen_content(){
+    console.log(0);
+    main_picture.src='grafika/Armia.jpg';
+}
+
+
 // **** EVENTS **** 
 button_fill_in_Rainhards_army.onclick = function(e){
-    var number_shieldbearers_R = input_Rainhards_shieldbearers.value,
-        number_crossbowmen_R = input_Rainhards_crossbowmen.value;
+    var numberShieldbearersR = input_Rainhards_shieldbearers.value,
+        numberCrossbowmenR = input_Rainhards_crossbowmen.value;
     view_Rainhards_army(number_shieldbearers_R, number_crossbowmen_R);  
 }
 
 button_fill_in_Galahads_army.onclick = function(e){
-    var number_shieldbearers_G = input_Galahads_shieldbearers.value,
-        number_crossbowmen_G = input_Galahads_crossbowmen.value;
-    view_Galahads_army(number_shieldbearers_G, number_crossbowmen_G);  
+    var numberShieldbearersG = input_Galahads_shieldbearers.value,
+        numberCrossbowmenG = input_Galahads_crossbowmen.value;
+    view_Galahads_army(numberShieldbearersG, numberCrossbowmenG);  
 }
+
 
 button_start_the_battle.onclick = function(e){
-
+    console.log(5)
+    change_screen_content();
 }
+
+button_start_the_battle.onclick = change_screen_content();
+
+button_start_the_battle.onclick = console.log(5);
+
+
